e2617b36aa6a10b75183e3677013bbf7684b5c9c
Usama-Saifullah <22-bsit-20@saus.edu.pk>
2025-04-20 02:20:21 +0500

    Reorganized project structure

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/saif/projects/turtlebot3-autonav/catkin_ws/src
+# Build directory: /home/saif/projects/turtlebot3-autonav/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
